17b05a37ba41ef66fcc5b63453547382eee99b5e
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-06-23 07:48:58 +0100

    Add code for index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// Wait for the DOM to finish loading before loading page
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+	let buttons = document.getElementsByClassName("button");
+
+	for (let button of buttons) {
+		button.addEventListener("click", function() {
+			if (this.getAttribute("data-type") === "home") {
+				indexPage();
+			} else {
+				
+			}
+		});
+	}
+	indexPage();
+});
 function clickByCity() {
 
 }
@@ -19,5 +36,17 @@ function previousPage() {
 }
 
 function indexPage() {
-    
+    let insert = `<h2 class="center">Display Prayer Times</h2>
+    <div class="btn-box" data-type="by">
+        <div class="btn button">
+            <span>By City</span>
+        </div>
+    </div>
+    <div class="btn-box">
+        <div class="btn button">
+            <span>By Location</span>
+        </div>
+    </div>`;
+    let html = document.getElementById('display');
+    html.innerHTML = insert;
 }
