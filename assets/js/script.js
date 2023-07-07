b65afe4d51441ac12bdc194d455862b08f698093
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-07-07 09:51:04 +0100

    Add timer dail html code

--- assets/js/script.js
+++ assets/js/script.js
@@ -370,7 +370,15 @@ function changeActive(time){
 function insertTimer(){
     let element = document.getElementsByClassName('prayers active');
     if (element[0].children.length < 4){
-        element[0].innerHTML += `<div class="timer"></div>`;
+        element[0].innerHTML += `
+        <div class="timer">
+            <div class ="percent" style="--num:1;">
+                <svg>
+                    <circle cx="40" cy="40" r="38"></circle>
+                    <circle cx="40" cy="40" r="38"></circle>
+                </svg>
+            </div>
+        </div>`;
     }
 }
  /**
